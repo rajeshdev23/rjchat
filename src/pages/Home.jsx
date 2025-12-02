678359aa7333a5b0a80b1b7a49e0cbab07f69ff0
rajesh <rajesh.kumar@delostylestudio.com>
2025-12-02 17:24:44 +0530

    creating responsive view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import "../style.css"
 
 const Home = () => {
   return (
-    <main className="text-white w-full h-[100vh] overflow-hidden grid grid-cols-1 md:grid-cols-12 bg-gray-900">
+    <main className="text-white w-full h-[100vh] overflow-auto grid grid-cols-1 md:grid-cols-12 bg-gray-900">
 
       {/* Sidebar */}
       <aside className="col-span-12 md:col-span-4 bg-gray-800 border-r border-gray-700">
