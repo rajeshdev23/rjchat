a04d910699e848aca5b02494c9822fae69775509
rajesh <rajesh.kumar@delostylestudio.com>
2025-12-02 17:05:25 +0530

    upading for responsiveview

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,15 +5,15 @@ import "../style.css"
 
 const Home = () => {
   return (
-    <main className="text-white grid  w-full  h-[100vh] overflow-y-hidden grid-cols-12 bg-gray-900">
+    <main className="text-white w-full h-[100vh] overflow-hidden grid grid-cols-1 md:grid-cols-12 bg-gray-900">
 
       {/* Sidebar */}
-      <aside className="col-span-4 bg-gray-800 border-r border-gray-700  ">
+      <aside className="col-span-12 md:col-span-4 bg-gray-800 border-r border-gray-700">
         <Sidebar />
       </aside>
 
       {/* Chat Area */}
-      <section className="col-span-8 bg-gray-900 ">
+      <section className="col-span-12 md:col-span-8 bg-gray-900">
         <Chat />
       </section>
 
